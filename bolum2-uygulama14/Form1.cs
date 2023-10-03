90c4048feab902af9a45b4a1422a153a9e3d5099
beyza37 <145428578+beyza37@users.noreply.github.com>
2023-10-03 11:16:22 +0300

    bolum2-uygulama14

---
+++ bolum2-uygulama14/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum2_uygulama14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1;
+            int sayi2;
+            int toplam;
+            sayi1 = 8;
+            sayi2 = 64;
+            toplam = sayi1 + sayi2;
+            MessageBox.Show(toplam.ToString());
+
+        }
+    }
+}
